9ce197a55abd927706fed47d3091cf23296ffbb7
chag60460 <75556593+chag60460@users.noreply.github.com>
2026-05-30 21:51:55 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        min_valid_speed = right
+
+        while left <= right:
+            time = 0
+            midpoint_speed = (left + right) // 2
+
+            for pile in piles:
+                time += math.ceil(pile / midpoint_speed)
+            
+            if time > h:
+                left = midpoint_speed + 1
+            else:
+                min_valid_speed = midpoint_speed
+                right = midpoint_speed - 1
+        
+        return min_valid_speed
