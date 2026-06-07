9d4f5acf350e7f44f3adbabf3f19b0d0857a5c95
chag60460 <75556593+chag60460@users.noreply.github.com>
2026-06-07 16:16:11 -0700

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        s1_count, s2_count = [0] * 26, [0] * 26
+
+        #initial count array and compute initial match, window left = 0, right = len(s1)
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+
+        for i in range(26):
+            matches += (1 if s1_count[i] == s2_count[i] else 0)
+        
+        left = 0
+        for right in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[right]) - ord('a')
+            s2_count[index] += 1
+
+            if s2_count[index] == s1_count[index]:
+                matches += 1
+            elif s2_count[index] - 1 == s1_count[index]: #this means previously they were equal and now with the new window they aren't
+                matches -= 1
+            
+            index = ord(s2[left]) - ord('a')
+            s2_count[index] -= 1
+
+            if s2_count[index] == s1_count[index]:
+                matches += 1
+            elif s2_count[index] + 1 == s1_count[index]: 
+                matches -= 1
+            left += 1
+
+        return matches == 26
